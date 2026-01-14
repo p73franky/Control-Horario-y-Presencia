e6ec20b8f3bb68b68129167c269b49a837de471d
p73franky <158572193+p73franky@users.noreply.github.com>
2026-01-14 13:45:48 +0100

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,17 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
-const db = new sqlite3.Database('./fichajes.db');
+const dbPath = path.join(__dirname, 'fichajes.db');
+
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Error al conectar con la base de datos', err);
+  } else {
+    console.log('Base de datos conectada');
+  }
+});
+
 
 db.serialize(() => {
   db.run(`
