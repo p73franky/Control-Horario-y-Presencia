39dc52b4cda19c4f895426646602d6299c657e0e
Lexsser <danielprosito333@gmail.com>
2026-01-21 14:08:00 +0100

    Subo el correcto fichar_salida.php

--- fichar_salida.php
+++ fichar_salida.php
@@ -1,9 +1,17 @@
 <?php
 include "db.php";
-$id = $_SESSION['empleado']['id'];
+
+$id    = $_SESSION['empleado']['id'];
 $fecha = date("Y-m-d");
 
-$conn->query("UPDATE fichajes SET hora_salida=CURTIME()
-              WHERE empleado_id=$id AND fecha='$fecha'");
+// Solo guardar salida si aún no existe
+$conn->query("
+    UPDATE fichajes 
+    SET hora_salida = CURTIME()
+    WHERE empleado_id = $id
+    AND fecha = '$fecha'
+    AND hora_salida IS NULL
+");
 
 header("Location: index.php");
+
